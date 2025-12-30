694c553c0c053e7c7dd0b9848c01373c8d9bf403
ghpreeti <preetisinghitt@gamil.com>
2025-12-30 16:53:33 +0530

    classPrac

---
+++ LPU_BL/LPU_Common/GenericClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LPU_Common
+{
+    /// <summary>
+    /// Custom generic class created for demo with custom generic method
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class GenericClass<T>
+    {
+        public void SwapMe(ref T obj1,ref T obj2) { 
+            T temp = obj1;
+            obj1 = obj2;
+            obj2 = temp;
+        }
+    }
+}
---
+++ LPU_BL/LPU_Common/IEmployee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LPU_Common
+{
+    interface IEmployee<T>:IRepo<T>
+    {
+        List<T> ShowAllFemaleEmployee();
+        List<T> ShowAllMaleEmployee();
+        List<T> ShowAllEmployeeWithAgeAbove40();
+    }
+}
