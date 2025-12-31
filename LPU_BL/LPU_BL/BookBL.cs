5885e345bed5c98529b6871d9f618eedfe0e7801
ghpreeti <preetisinghitt@gamil.com>
2025-12-31 18:21:10 +0530

    LayerStructurePractice

---
+++ LPU_BL/LPU_BL/BookBL.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using LPU_Common;
+using LPU_Entity;
+using LPU_DAL;
+using LPU_Exceptions;
+
+namespace LPU_BL
+{
+    public class BookBL : IRepo<Book>
+    {
+        BookDAO bDao = null;
+
+        public BookBL()
+        {
+            bDao = new BookDAO();
+        }
+
+        // DELETE
+        public bool DeleteDetails(int id)
+        {
+            bool flag = false;
+            try
+            {
+                if (id != 0)
+                {
+                    flag = bDao.DeleteDetails(id);
+                }
+                else
+                {
+                    throw new BookException("Invalid Book ID");
+                }
+            }
+            catch (BookException e)
+            {
+                throw e;
+            }
+            return flag;
+        }
+
+        // CREATE
+        public bool AddDetails(Book obj)
+        {
+            bool flag = false;
+            try
+            {
+                if (obj != null)
+                {
+                    flag = bDao.AddDetails(obj);
+                }
+                else
+                {
+                    throw new BookException("Invalid Book Data");
+                }
+            }
+            catch (BookException e)
+            {
+                throw e;
+            }
+            return flag;
+        }
+
+        // READ BY ID
+        public Book ShowDetailsByID(int id)
+        {
+            Book b = null;
+            try
+            {
+                b = bDao.ShowDetailsByID(id);
+            }
+            catch (BookException e)
+            {
+                throw e;
+            }
+            return b;
+        }
+
+        // READ ALL
+        public List<Book> ShowAll()
+        {
+            return bDao.ShowAll();
+        }
+
+        // UPDATE
+        public bool UpdateDetails(int id, Book obj)
+        {
+            bool flag = false;
+            try
+            {
+                if (id > 0 && obj != null)
+                {
+                    flag = bDao.UpdateDetails(id, obj);
+                }
+                else
+                {
+                    throw new BookException("Invalid input data");
+                }
+            }
+            catch (BookException e)
+            {
+                throw e;
+            }
+            return flag;
+        }
+    }
+}
--- LPU_BL/LPU_DAL/BookDAO.cs
+++ LPU_BL/LPU_DAL/BookDAO.cs
@@ -1,37 +1,82 @@
-﻿using LPU_Common;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Text;
 using LPU_Common;
 using LPU_Entity;
+using LPU_Exceptions;
 
 namespace LPU_DAL
 {
     public class BookDAO : IRepo<Book>
     {
+        static List<Book> bookList = null;
+
+        public BookDAO()
+        {
+            if (bookList == null)
+            {
+                bookList = new List<Book>()
+                {
+                    new Book { ISBNNo = 1, Title = "C# Basics", Author = "MS", Price = 450 },
+                    new Book { ISBNNo = 2, Title = "ASP.NET", Author = "Scott", Price = 550 },
+                    new Book { ISBNNo = 3, Title = "Data Structures", Author = "Mark", Price = 600 },
+                    new Book { ISBNNo = 4, Title = "Java Programming", Author = "Herbert", Price = 500 },
+                    new Book { ISBNNo = 5, Title = "Python Essentials", Author = "Guido", Price = 480 },
+                    new Book { ISBNNo = 6, Title = "Operating Systems", Author = "Silberschatz", Price = 650 },
+                    new Book { ISBNNo = 7, Title = "Database Systems", Author = "Elmasri", Price = 700 },
+                    new Book { ISBNNo = 8, Title = "Computer Networks", Author = "Tanenbaum", Price = 620 },
+                    new Book { ISBNNo = 9, Title = "Software Engineering", Author = "Pressman", Price = 580 }
+
+                };
+            }
+        }
+
         public bool AddDetails(Book obj)
         {
-            throw new NotImplementedException();
+            if (obj != null)
+            {
+                bookList.Add(obj);
+                return true;
+            }
+            throw new BookException("Invalid book data");
         }
 
         public bool DeleteDetails(int id)
         {
-            throw new NotImplementedException();
+            Book b = bookList.Find(x => x.ISBNNo == id);
+            if (b != null)
+            {
+                bookList.Remove(b);
+                return true;
+            }
+            throw new BookException("Book not found");
         }
 
         public List<Book> ShowAll()
         {
-            throw new NotImplementedException();
+            return bookList;
         }
 
         public Book ShowDetailsByID(int id)
         {
-            throw new NotImplementedException();
+            Book b = bookList.Find(x => x.ISBNNo == id);
+            if (b != null)
+            {
+                return b;
+            }
+            throw new BookException("Book not found");
         }
 
         public bool UpdateDetails(int id, Book obj)
         {
-            throw new NotImplementedException();
+            Book b = bookList.Find(x => x.ISBNNo == id);
+            if (b != null && obj != null)
+            {
+                b.Title = obj.Title;
+                b.Author = obj.Author;
+                b.Price = obj.Price;
+                return true;
+            }
+            throw new BookException("Unable to update book");
         }
     }
 }
