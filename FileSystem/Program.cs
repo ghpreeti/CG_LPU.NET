bd048114e3233d5471b294de1cf4f66c0f80ab60
ghpreeti <preetisinghitt@gamil.com>
2026-01-10 15:50:13 +0530

    10-jan Practice

---
+++ FileSystem/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using FileSystem;
+
+
+DirectoryDemo dObj = new DirectoryDemo();
+
+//dObj.DirectoryDemoFunc("LPU");
+ //dObj.DriveInfoFunc("F:\\");
+ //dObj.PathDemoFunc();
+
+FileStreamDemo fsDemoObj  = new FileStreamDemo();
+//fsDemoObj.CreateFile(@"E:\disk d\fullStack\trainingCapgemeni\FileSystem\SampleFile.txt");
+ fsDemoObj.ReadFile(@"E:\disk d\fullStack\trainingCapgemeni\FileSystem\DirectoryDemo.cs");
+
+
