4aa735a272d18d144ed05a82ec08c518add6f7a4
ghpreeti <preetisinghitt@gamil.com>
2026-01-29 20:53:40 +0530

    NUNIT

---
+++ CodeForUnitTesting/NUnitTestProject1/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using CalculatorService;
+
+namespace NUnitTestProject1
+{
+    public class Tests
+    {
+        Calculator calObj = null;
+        [SetUp]
+        public void Setup()
+        {
+            calObj = new Calculator();
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+        [TearDown]
+        public void Reset() {
+            calObj = null;
+        }
+    }
+}
