47cb64e6c56d9a29cb85f4d379f4669bce63af92
ghpreeti <preetisinghitt@gamil.com>
2026-02-11 11:37:35 +0530

    assesment done on 10 feb 2026

---
+++ UniverSity Course Registration System/UniverSity Course Registration System/Student.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace University_Course_Registration_System
+{
+    // =========================
+    // Student Class
+    // =========================
+    public class Student
+    {
+        public string StudentId { get; private set; }
+        public string Name { get; private set; }
+        public string Major { get; private set; }
+        public int MaxCredits { get; private set; }
+
+        public List<string> CompletedCourses { get; private set; }
+        public List<Course> RegisteredCourses { get; private set; }
+
+        public Student(string id, string name, string major, int maxCredits = 18, List<string> completedCourses = null)
+        {
+            StudentId = id;
+            Name = name;
+            Major = major;
+            MaxCredits = maxCredits;
+            CompletedCourses = completedCourses ?? new List<string>();
+            RegisteredCourses = new List<Course>();
+        }
+
+        public int GetTotalCredits()
+        {
+            // TODO: Return sum of credits of all RegisteredCourses
+            return RegisteredCourses.Sum(c => c.Credits);
+
+            //throw new NotImplementedException();
+        }
+
+        public bool CanAddCourse(Course course)
+        {
+            // TODO:
+            // 1. Course should not already be registered
+            // 2. Total credits + course credits <= MaxCredits
+            // 3. Course prerequisites must be satisfied
+
+            if(RegisteredCourses.Any(c=>c.CourseCode == course.CourseCode)) return false;
+
+            if(GetTotalCredits()+course.Credits > MaxCredits) return false;
+
+            if (!course.HasPrerequisites(CompletedCourses.Select(c => c).ToList()
+            )) { return false; }
+            else { return true; }
+
+            //throw new NotImplementedException();
+        }
+
+        public bool AddCourse(Course course)
+        {
+            // TODO:
+            // 1. Call CanAddCourse
+            // 2. Check course capacity
+            // 3. Add course to RegisteredCourses
+            // 4. Call course.EnrollStudent()
+
+            CanAddCourse(course);
+            if(course.IsFull()) return false;
+            RegisteredCourses.Add(course);
+            course.EnrollStudent();
+            return true;
+
+            //throw new NotImplementedException();
+        }
+
+        public bool DropCourse(string courseCode)
+        {
+            // TODO:
+            // 1. Find course by code
+            // 2. Remove from RegisteredCourses
+            // 3. Call course.DropStudent()
+
+            if(!RegisteredCourses.Any(c=>c.CourseCode == courseCode)) return false;
+            var course = RegisteredCourses.First(c=>c.CourseCode == courseCode);
+            RegisteredCourses.Remove(course);
+            course.DropStudent();
+            return true;
+
+            //throw new NotImplementedException();
+        }
+
+        public void DisplaySchedule()
+        {
+            // TODO:
+            // Display course code, name, and credits
+            // If no courses registered, display appropriate message
+
+            var query = RegisteredCourses.Select(e => $"{e.CourseCode} - {e.CourseName} - {e.Credits}").ToList();
+            
+                if(RegisteredCourses == null)
+                {
+                    Console.WriteLine(" no courses registered");
+                }
+                else
+                {
+                foreach (var c in query)
+                {
+                    Console.WriteLine(c);
+                }
+            }
+
+           // throw new NotImplementedException();
+        }
+    }
+}
