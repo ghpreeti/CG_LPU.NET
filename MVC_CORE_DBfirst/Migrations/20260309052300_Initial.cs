59c2f0738eab64b11941d126595f59e616676580
ghpreeti <preetisinghitt@gamil.com>
2026-03-10 18:19:34 +0530

    web api with and without CRUD practice

---
+++ MVC_CORE_DBfirst/Migrations/20260309052300_Initial.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MVC_CORE_DBfirst.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Customers",
+                columns: table => new
+                {
+                    CustomerID = table.Column<string>(type: "nchar(5)", fixedLength: true, maxLength: 5, nullable: false),
+                    CompanyName = table.Column<string>(type: "nvarchar(40)", maxLength: 40, nullable: false),
+                    ContactName = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: true),
+                    ContactTitle = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: true),
+                    Address = table.Column<string>(type: "nvarchar(60)", maxLength: 60, nullable: true),
+                    City = table.Column<string>(type: "nvarchar(15)", maxLength: 15, nullable: true),
+                    Region = table.Column<string>(type: "nvarchar(15)", maxLength: 15, nullable: true),
+                    PostalCode = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: true),
+                    Country = table.Column<string>(type: "nvarchar(15)", maxLength: 15, nullable: true),
+                    Phone = table.Column<string>(type: "nvarchar(24)", maxLength: 24, nullable: true),
+                    Fax = table.Column<string>(type: "nvarchar(24)", maxLength: 24, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customers", x => x.CustomerID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    ProductID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ProductName = table.Column<string>(type: "nvarchar(40)", maxLength: 40, nullable: false),
+                    SupplierID = table.Column<int>(type: "int", nullable: true),
+                    CategoryID = table.Column<int>(type: "int", nullable: true),
+                    QuantityPerUnit = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: true),
+                    UnitPrice = table.Column<decimal>(type: "money", nullable: true, defaultValue: 0m),
+                    UnitsInStock = table.Column<short>(type: "smallint", nullable: true, defaultValue: (short)0),
+                    UnitsOnOrder = table.Column<short>(type: "smallint", nullable: true, defaultValue: (short)0),
+                    ReorderLevel = table.Column<short>(type: "smallint", nullable: true, defaultValue: (short)0),
+                    Discontinued = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.ProductID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Orders",
+                columns: table => new
+                {
+                    OrderID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CustomerID = table.Column<string>(type: "nchar(5)", fixedLength: true, maxLength: 5, nullable: true),
+                    EmployeeID = table.Column<int>(type: "int", nullable: true),
+                    OrderDate = table.Column<DateTime>(type: "datetime", nullable: true),
+                    RequiredDate = table.Column<DateTime>(type: "datetime", nullable: true),
+                    ShippedDate = table.Column<DateTime>(type: "datetime", nullable: true),
+                    ShipVia = table.Column<int>(type: "int", nullable: true),
+                    Freight = table.Column<decimal>(type: "money", nullable: true, defaultValue: 0m),
+                    ShipName = table.Column<string>(type: "nvarchar(40)", maxLength: 40, nullable: true),
+                    ShipAddress = table.Column<string>(type: "nvarchar(60)", maxLength: 60, nullable: true),
+                    ShipCity = table.Column<string>(type: "nvarchar(15)", maxLength: 15, nullable: true),
+                    ShipRegion = table.Column<string>(type: "nvarchar(15)", maxLength: 15, nullable: true),
+                    ShipPostalCode = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: true),
+                    ShipCountry = table.Column<string>(type: "nvarchar(15)", maxLength: 15, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orders", x => x.OrderID);
+                    table.ForeignKey(
+                        name: "FK_Orders_Customers",
+                        column: x => x.CustomerID,
+                        principalTable: "Customers",
+                        principalColumn: "CustomerID");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "City",
+                table: "Customers",
+                column: "City");
+
+            migrationBuilder.CreateIndex(
+                name: "CompanyName",
+                table: "Customers",
+                column: "CompanyName");
+
+            migrationBuilder.CreateIndex(
+                name: "PostalCode",
+                table: "Customers",
+                column: "PostalCode");
+
+            migrationBuilder.CreateIndex(
+                name: "Region",
+                table: "Customers",
+                column: "Region");
+
+            migrationBuilder.CreateIndex(
+                name: "CustomerID",
+                table: "Orders",
+                column: "CustomerID");
+
+            migrationBuilder.CreateIndex(
+                name: "CustomersOrders",
+                table: "Orders",
+                column: "CustomerID");
+
+            migrationBuilder.CreateIndex(
+                name: "EmployeeID",
+                table: "Orders",
+                column: "EmployeeID");
+
+            migrationBuilder.CreateIndex(
+                name: "EmployeesOrders",
+                table: "Orders",
+                column: "EmployeeID");
+
+            migrationBuilder.CreateIndex(
+                name: "OrderDate",
+                table: "Orders",
+                column: "OrderDate");
+
+            migrationBuilder.CreateIndex(
+                name: "ShippedDate",
+                table: "Orders",
+                column: "ShippedDate");
+
+            migrationBuilder.CreateIndex(
+                name: "ShippersOrders",
+                table: "Orders",
+                column: "ShipVia");
+
+            migrationBuilder.CreateIndex(
+                name: "ShipPostalCode",
+                table: "Orders",
+                column: "ShipPostalCode");
+
+            migrationBuilder.CreateIndex(
+                name: "CategoriesProducts",
+                table: "Products",
+                column: "CategoryID");
+
+            migrationBuilder.CreateIndex(
+                name: "CategoryID",
+                table: "Products",
+                column: "CategoryID");
+
+            migrationBuilder.CreateIndex(
+                name: "ProductName",
+                table: "Products",
+                column: "ProductName");
+
+            migrationBuilder.CreateIndex(
+                name: "SupplierID",
+                table: "Products",
+                column: "SupplierID");
+
+            migrationBuilder.CreateIndex(
+                name: "SuppliersProducts",
+                table: "Products",
+                column: "SupplierID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Orders");
+
+            migrationBuilder.DropTable(
+                name: "Products");
+
+            migrationBuilder.DropTable(
+                name: "Customers");
+        }
+    }
+}
---
+++ WebAPI_Demo/ASPCoreWebApi/Controllers/FruitsAPIController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASPCoreWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FruitsAPIController : ControllerBase
+    {
+        public List<string> fruits = new List<string>() { "Apple", "Banana", "Cherry", "Mango", "Grapes" };
+
+        [HttpGet]
+        public List<string> GetFruits() { return fruits; }
+
+        [HttpGet("{id}")]
+        public string GetFruitsByIndex(int id) { return fruits.ElementAt(id); }
+    }
+}
