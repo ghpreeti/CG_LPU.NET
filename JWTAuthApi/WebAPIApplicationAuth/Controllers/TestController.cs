b0f6c32581b3881a50af5f74f6cfac9ddd9e969c
ghpreeti <preetisinghitt@gamil.com>
2026-03-13 00:48:29 +0530

    Authentication done using JWT & identity in aspCoreApi

---
+++ JWTAuthApi/WebAPIApplicationAuth/Controllers/TestController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPIApplicationAuth.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetMessage()
+        {
+            return Ok("You are authorized to access this API");
+        }
+    }
+}
---
+++ JWTAuthApi/WebAPIApplicationAuth/Models/ApplicationUser.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace WebAPIApplicationAuth.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
