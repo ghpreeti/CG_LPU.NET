a11b993fa62214e7aa75aef54f45c2c41a1d56fa
ghpreeti <preetisinghitt@gamil.com>
2026-03-14 18:15:52 +0530

    Keep only StudentManagementSystem for assessment

--- WebApiCRUDusingMVC/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace WebApiCRUDusingMVC.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
