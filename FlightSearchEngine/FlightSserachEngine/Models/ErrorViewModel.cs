cc9f4d57b97d74a2f74dad24f403263508cddbf8
ghpreeti <preetisinghitt@gamil.com>
2026-03-17 00:07:15 +0530

    Clean branch: keep only ProjectManagementSystem

--- FlightSearchEngine/FlightSserachEngine/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace FlightSserachEngine.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
--- SQLServer/SQLQuery1.sql
+++
@@ -1,5 +0,0 @@
---CREATE SCHEMA PankajBatch
-create table Person(Id int, Name varchar(30) Not Null,Age int,Address varchar(50), PhoneNo varchar(10))
-insert into Person values(1,'Preeti',20,'ABC',7878787878)
-
-select * from Person
